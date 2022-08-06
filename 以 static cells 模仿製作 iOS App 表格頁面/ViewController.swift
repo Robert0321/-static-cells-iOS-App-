9d5843c65084e84ca8ea0df37eeebc37cb2c637f
Robert0321 <shat0806449@gmail.com>
2022-08-06 11:12:24 +0800

    Initial Commit

---
+++ 以 static cells 模仿製作 iOS App 表格頁面/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  以 static cells 模仿製作 iOS App 表格頁面
+//
+//  Created by robert on 2022/8/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
